595610c03cb142fd7f328380556c3576198f2c74
catsonmars <meganvineyard83@gmail.com>
2024-02-23 10:07:35 -0700

    Add files via upload

---
+++ tests.py
@@ -0,0 +1,13 @@
+import unittest
+import task
+import os.
+
+class TestCase(unittest.TestCase):
+
+    def test1(self):
+        expected = "Hello World"
+        self.assertEqual(task.my_func(), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
