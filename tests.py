9e727cd3215daf04b9290c7a11e9ec5a4e28d006
catsonmars <meganvineyard83@gmail.com>
2024-02-23 10:26:57 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,4 +1,11 @@
 import unittest
+import os
+
+class TestCase(unittest.TestCase):
+
+    def test_test(self):
+        cwd = os.getcwd()
+        print(cwd)
 
 if __name__ == '__main__':
     unittest.main()
